90027704acee6d114fdd0b8a2f52715592fda989
elderluiz-dev <elderluiz72@gmail.com>
2026-08-02 19:32:27 -0300

    added second matrix list

--- matrix/matrix2/10.c
+++ matrix/matrix2/10.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main(){
-  int m[3][3];
+  int m[10][10];
   
-  for(int i = 0; i < 3; i++){
-    for(int j = 0; j < 3; j++){
+  for(int i = 0; i < 10; i++){
+    for(int j = 0; j < 10; j++){
       scanf("%d", &m[i][j]);
     }
   }
   
-  for(int i = 0; i < 3; i++){
-    for(int j = 0; j < 3; j++){
-      if((i + j) < (3 - 1)){
+  for(int i = 0; i < 10; i++){
+    for(int j = 0; j < 10; j++){
+      if((i + j) < (10 - 1)){
         printf("%d", m[i][j]);
       }
     }
--- matrix/matrix2/4.c
+++ matrix/matrix2/4.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main(){
-  int m[3][3];
+  int m[10][10];
   
-  for(int i = 0; i < 3; i++){
-    for(int j = 0; j < 3; j++){
+  for(int i = 0; i < 10; i++){
+    for(int j = 0; j < 10; j++){
       scanf("%d", &m[i][j]);
     }
   }
   
-  for(int i = 0; i < 3; i++){
-    for(int j = 0; j < 3; j++){
+  for(int i = 0; i < 10; i++){
+    for(int j = 0; j < 10; j++){
       if(i < j){
         printf("%d", m[i][j]);
       }
