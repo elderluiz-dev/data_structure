cf087487917de0ca728451fd0b40a3e4ce4eb663
elderluiz-dev <elderluiz72@gmail.com>
2026-08-15 18:16:20 -0300

    finished list

---
+++ dynamic_memory_alloc/11.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int n = 50000000;
+  
+  int *p = malloc(n * sizeof(*p));
+  if(p == NULL){
+    printf("Erro de alocação!\n");
+    return EXIT_FAILURE;
+  }
+
+  free(p);
+  return 0;
+}
